7513a99c9ba86a1aacfbdc0685a7052ea62f7b17
oyupanqui <osmar30asis@gmail.com>
2023-10-13 22:41:58 -0500

    added prevent default

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 const giphyKey = "tO9l2feYc3Uot5303yfSlDOFs3GJzFdz"
 const weatherAPI = "4968ce87e17e44e083313750231310"
 
+const submitBtn = document.getElementById("submit")
+
+submitBtn.addEventListener("click", function (event) {
+    event.preventDefault()
+})
