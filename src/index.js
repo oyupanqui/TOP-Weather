d99a321e1935fa7335a2ede118e037739476f8c7
oyupanqui <osmar30asis@gmail.com>
2023-10-14 21:37:32 -0500

    testing fetch and weather api

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ const weatherAPI = "4968ce87e17e44e083313750231310"
 
 const submitBtn = document.getElementById("submit")
 
+async function getData (prompt, key) {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${prompt}&aqi=no`
+    const response = await fetch(url, {
+        mode: "cors"
+    }).then(function (response) {
+        return response.json()
+    }).then(function (response) {
+        console.log(response)
+    })
+}
+
 submitBtn.addEventListener("click", function (event) {
     event.preventDefault()
 
     const prompt = document.getElementById("prompt").value
-    console.log(prompt)
+
+    getData(prompt, weatherAPI)
 })
 
