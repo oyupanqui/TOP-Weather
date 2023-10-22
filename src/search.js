e6efcdd1b904259012274e3aa635c204875d68e0
oyupanqui <osmar30asis@gmail.com>
2023-10-22 00:40:52 -0500

    added getweatherdata function

--- src/search.js
+++ src/search.js
@@ -1,3 +1,11 @@
-function searchFunction () {
+import { weatherAPI } from './keys'
 
+export async function getWeatherData (prompt) {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${weatherAPI}&q=${prompt}&aqi=no`
+    const response = await fetch(url, {
+        mode: "cors"
+    }).then(function (response) {
+        return response.json()
+    })
+    return response
 }
