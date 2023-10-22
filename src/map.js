bda1b895c16627ac9cf6b5036451945bdf8265d4
oyupanqui <osmar30asis@gmail.com>
2023-10-22 16:47:43 -0500

    created loadMap function

--- src/map.js
+++ src/map.js
@@ -1,7 +1,8 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+export function loadMap () {
+    const map = L.map('map').setView([0, 0], 0)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
+    }).addTo(map);
+    return map
+}
