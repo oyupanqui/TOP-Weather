bb0d0ed3fbbe0b7161b27c327f9c26ffc07e2581
oyupanqui <osmar30asis@gmail.com>
2023-10-22 22:29:45 -0500

    added popup with location name

--- src/map.js
+++ src/map.js
@@ -2,10 +2,25 @@ let map;
 
 export function loadMap () {
     map = L.map('map').setView([0, 0], 0)
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+        attribution: '&copy; OpenStreeMap'
+    }).addTo(map)
+    const google = L.tileLayer('https://mt.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
+        maxZoom: 19,
+        attribution: '&copy; Google'
+    })
+    const arcgis = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        maxZoom: 19,
+        attribution: '&copy; ESRI'
+    })
+
+    const layerControl = L.control.layers({
+        "OSM": osm,
+        "Google": google,
+        "ESRI": arcgis
+    }).addTo(map)
+
     return map
 }
 
@@ -14,5 +29,7 @@ export function mapListener (response) {
     const lon = response.location.lon
     const marker = L.marker([lat, lon]).addTo(map)
     map.setView([lat, lon], 12)
+
+    marker.bindPopup(response.location.name).openPopup()
     return marker
 }
