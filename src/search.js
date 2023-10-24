2b47c2ed08149cba93b865b73e0dc1942dfecb3b
oyupanqui <osmar30asis@gmail.com>
2023-10-24 00:56:58 -0500

    created function to add giphy data

--- src/search.js
+++ src/search.js
@@ -1,7 +1,17 @@
-import { weatherAPI } from './keys'
+import { weatherAPI, giphyKey } from './keys'
 
 export async function getWeatherData (prompt) {
     const url = `https://api.weatherapi.com/v1/current.json?key=${weatherAPI}&q=${prompt}&aqi=no`
+    const response = await fetch(url, {
+        mode: "cors"
+    }).then(function (response) {
+        return response.json()
+    })
+    return response
+}
+
+export async function getGiphyData (prompt) {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${String(prompt).replaceAll(" ", "+")}&api_key=${giphyKey}&limit=10`
     const response = await fetch(url, {
         mode: "cors"
     }).then(function (response) {
